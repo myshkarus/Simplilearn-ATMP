350d9ba9d8429c6a698f8652cfddf5c5021cf6a8
Mykhailo Shpilienko <m.shpilenko@gmail.com>
2022-09-16 13:47:44 +0300

    Modification to LoanTest class and testng.xml

--- Phase2/TestngProject/src/test/LoanTest.java
+++ Phase2/TestngProject/src/test/LoanTest.java
@@ -7,17 +7,17 @@ import org.testng.annotations.Test;
 @Test(groups = {"regression"})
 public class LoanTest extends BaseClass {
 	
-	@Test(groups = {"sanity"})
+	@Test(groups = {"sanity"}, description="This is to validate Home Loan", priority=1)
 	public void HomeLoanTest() {
 		System.out.println("Inside Home Loan Test");
 	}
 
-	@Test
+	@Test(enabled=false)
 	public void PersonalLoanTest() {
 		System.out.println("Inside Personal Loan Test");
 	}
 	
-	@Test
+	@Test(priority=0)
 	public void PersonalLoanTest1() {
 		System.out.println("Inside Personal Loan Test 1");
 	}
