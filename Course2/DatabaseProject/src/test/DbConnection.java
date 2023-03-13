f633e2139073a4d197f4df7a60d1f781e83dfe8b
Mykhailo Shpilienko <m.shpilenko@gmail.com>
2023-03-14 01:13:47 +0200

    DatabaseProject updated

--- Course2/DatabaseProject/src/test/DbConnection.java
+++ Course2/DatabaseProject/src/test/DbConnection.java
@@ -1,34 +1,33 @@
-package test; 
+package test;
 
-import java.sql.Connection; 
-import java.sql.DriverManager; 
-import java.sql.ResultSet; 
-import java.sql.SQLException; 
-import java.sql.Statement; 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
+public class DbConnection {
+	public static void main(String[] args) throws SQLException, ClassNotFoundException {
 
-public class DbConnection { 
-	public static void main(String[] args) throws SQLException, ClassNotFoundException { 
-		
 		// using dependency
-		Class.forName("com.mysql.cj.jdbc.Driver");   
-		 
+		Class.forName("com.mysql.cj.jdbc.Driver");
+
 		// create connection object
-		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ecommerce", "root", "admin");  
-		
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ecommerce", "root", "root");
+
 		// create statement
-		Statement stm = con.createStatement(); 
-		
+		Statement stm = con.createStatement();
+
 		// execute query
-		ResultSet result = stm.executeQuery("select * from eproduct"); 
-		
+		ResultSet result = stm.executeQuery("select * from eproduct");
+
 		// iterate result set
-		while(result.next()) { 
-			 
-			System.out.println(result.getInt("ID")); 
-			System.out.println(result.getString("name")); 
-			System.out.println(result.getFloat("price")); 
-			 
-		} 
-	} 
-} 
+		while (result.next()) {
+
+			System.out.println(result.getInt("ID"));
+			System.out.println(result.getString("name"));
+			System.out.println(result.getFloat("price"));
+
+		}
+	}
+}
